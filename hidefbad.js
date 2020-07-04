95e346908bb985cb0fcebd8fdb3e45fec6989077
chienj1 <b02202037@ntu.edu.tw>
2020-07-05 03:10:21 +0800

    update js

--- hidefbad.js
+++ hidefbad.js
@@ -1,15 +1,20 @@
 /*
 
 */
-var feeds = document.getElementsByClassName("pedkr2u6 tn0ko95a pnx7fd3z")[0].getElementsByTagName("div")[0].getElementsByTagName("div");
 
-var existing_feeds = 0;
-var i;
-
-for (i = 0; i< feeds.length; i++) 
-{
-  if ( feeds[i].dataset.pagelet == undefined ) { continue;}
-  else {existing_feeds ++;}
+function count_feeds() {
+  var feeds = document.getElementsByClassName("pedkr2u6 tn0ko95a pnx7fd3z")[0].getElementsByTagName("div")[0].getElementsByTagName("div");
+  var existing_feeds = 0;
+  var i;
+  for (i = 0; i< feeds.length; i++) 
+  {
+    if ( feeds[i].dataset.pagelet == undefined ) { continue;}
+    else {existing_feeds ++;}
+  }
+  
+  console.log(existing_feeds);
+  return 0;
 }
 
-console.log(existing_feeds);
+var timeoutID = window.setInterval( ()=>count_feeds(), 100);
+window.clearInterval(timeoutID)
