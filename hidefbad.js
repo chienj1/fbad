01db600b605817fac0f0856a8b088ae5042de318
chienj1 <b02202037@ntu.edu.tw>
2020-07-05 21:41:44 +0800

    update

--- hidefbad.js
+++ hidefbad.js
@@ -5,7 +5,7 @@
 function get_feeds() {
   return document.getElementsByClassName("pedkr2u6 tn0ko95a pnx7fd3z")[0].getElementsByTagName("div")[0].getElementsByTagName("div")
 }
-
+/*
 function count_feeds() {
   var feeds = get_feeds();
   var existing_feeds = 0;
@@ -16,9 +16,36 @@ function count_feeds() {
     else {existing_feeds ++;}
   }
   
-  console.log(existing_feeds);
-  return 0;
+  return existing_feeds;
 }
+*/
 
-var timeoutID = window.setInterval( ()=>count_feeds(), 100);
+/*
+function count_feeds() {
+  var feeds = get_feeds();
+  var existing_feeds = 0;
+  feeds.forEach( function(feed) { if ( feed.dataset.pagelet == undefined ) { continue;} 
+                                                         else {existing_feeds ++;}} )
+  return existing_feeds;
+}
+*/
+
+/*
+var timeoutID = window.setInterval( ()=>console.log(count_feeds()), 100);
 window.clearInterval(timeoutID)
+*/
+
+var mutationObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {  if (mutation.type == "childList" ) {console.log(mutation.target)};});}
+);
+
+mutationObserver.observe(document.documentElement, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+
+//mutationObserver.disconnect();
